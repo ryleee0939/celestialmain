f8fabbe805291a1c76343730b0731dd5baafd6b2
ryleee0939 <ryleemateroca@gmail.com>
2025-05-15 02:29:21 +0100

    Update library.lua

--- library.lua
+++ library.lua
@@ -101,7 +101,7 @@ local UI = ({
             accent = color3_rgb(189, 172, 255),
             risky = color3_rgb(200, 0, 0),
             background = color3_rgb(18, 18, 18),
-            outline = color3_rgb(43, 43, 43),
+            outline = color3_rgb(4, 4, 4),
             active = color3_rgb(255, 255, 255),
             inactive = color3_rgb(120, 120, 120)
         },
